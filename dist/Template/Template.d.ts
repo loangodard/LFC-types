49fb15002d82ba8077b5d4010a1f10a926a2232b
loangodard <loan.godard031@gmail.com>
2025-01-07 17:02:54 +0100

    adding template id

--- dist/Template/Template.d.ts
+++ dist/Template/Template.d.ts
@@ -1,6 +1,7 @@
 import { Face } from "./Face";
 export type TemplateStatus = 'active' | 'inactive';
 export interface Template {
+    id?: number;
     name: string;
     description?: string;
     faces: Face[];
--- src/Template/Template.ts
+++ src/Template/Template.ts
@@ -3,6 +3,7 @@ import { Face } from "./Face";
 export type TemplateStatus = 'active' | 'inactive';
 
 export interface Template {
+    id?: number;
     name: string;
     description?: string;
     faces: Face[];
