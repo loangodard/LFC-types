cf707b03156d117cd76e51a2cb955d37ccd571cc
loangodard <loan.godard031@gmail.com>
2025-01-07 16:43:29 +0100

    adding template.status

--- dist/Template/Template.d.ts
+++ dist/Template/Template.d.ts
@@ -1,6 +1,8 @@
 import { Face } from "./Face";
+export type TemplateStatus = 'active' | 'inactive';
 export interface Template {
     name: string;
     description?: string;
     faces: Face[];
+    status: TemplateStatus;
 }
--- src/Template/Template.ts
+++ src/Template/Template.ts
@@ -1,7 +1,10 @@
 import { Face } from "./Face";
 
+export type TemplateStatus = 'active' | 'inactive';
+
 export interface Template {
     name: string;
     description?: string;
     faces: Face[];
+    status: TemplateStatus;
 }
