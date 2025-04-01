f00b0d929b120d1dfd3ebf3b86b68fcef7338b79
loangodard <loan.godard031@gmail.com>
2025-04-01 11:33:07 +0200

    data to gen label

--- dist/Shipment/Carrier.d.ts
+++ dist/Shipment/Carrier.d.ts
@@ -3,8 +3,13 @@ export interface ItemToShip {
     sku: string;
     quantity: number;
 }
+export interface LabelData {
+    base64Label: string;
+    trackingNumber: string;
+    trackingUrl: string;
+}
 export interface Carrier {
     name: string;
     offers: string[];
-    generateLabel: (shipment: Shipment) => Promise<string>;
+    generateLabel: (shipment: Shipment) => Promise<LabelData>;
 }
--- src/Shipment/Carrier.ts
+++ src/Shipment/Carrier.ts
@@ -2,8 +2,15 @@ import { Shipment } from "./Shipment";
 
 export interface ItemToShip { sku: string; quantity: number; }
 
+
+export interface LabelData {
+    base64Label: string;
+    trackingNumber: string;
+    trackingUrl: string;
+};
+
 export interface Carrier {
     name: string;
     offers: string[];
-    generateLabel: (shipment: Shipment) => Promise<string>;
+    generateLabel: (shipment: Shipment) => Promise<LabelData>;
 }
